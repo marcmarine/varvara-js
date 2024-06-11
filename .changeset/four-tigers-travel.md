917d378231bf07108ef9d58f0af3954e44f9bb80
Marc Mariné <shenobi@gmail.com>
2024-06-11 10:14:02 +0200

    fix(css): Transform `@import` rules by inlining content

---
+++ .changeset/four-tigers-travel.md
@@ -0,0 +1,5 @@
+---
+"varvara-css": patch
+---
+
+Transform `@import` rules by inlining content.
--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,10 +1,11 @@
 import autoprefixer from 'autoprefixer'
 import nested from 'postcss-nested'
 import tailwindcss from 'tailwindcss'
+import atImport from 'postcss-import'
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [nested, tailwindcss, autoprefixer]
+  plugins: [atImport, nested, tailwindcss, autoprefixer]
 }
 
 export default config
