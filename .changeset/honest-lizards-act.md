0f9b4ced3743867d563bc47e37b98c639214b94a
Marc Mariné <shenobi@gmail.com>
2024-07-01 20:58:57 +0200

    feat(css): Add the prefix `va-` to class names

---
+++ .changeset/honest-lizards-act.md
@@ -0,0 +1,5 @@
+---
+"varvara-css": minor
+---
+
+Add the prefix `va-` to class names
--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -2,10 +2,19 @@ import autoprefixer from 'autoprefixer'
 import nested from 'postcss-nested'
 import tailwindcss from 'tailwindcss'
 import atImport from 'postcss-import'
+import prefixer from 'postcss-prefixer'
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [atImport, nested, tailwindcss, autoprefixer]
+  plugins: [
+    atImport,
+    nested,
+    tailwindcss,
+    autoprefixer,
+    prefixer({
+      prefix: 'va-'
+    })
+  ]
 }
 
 export default config
