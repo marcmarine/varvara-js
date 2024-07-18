95fef7d0b06c848cc412c9334468ca3894877190
Marc Mariné <shenobi@gmail.com>
2024-07-18 11:29:33 +0200

    feat(docs): Update styles and contents

--- docs/src/components/lib/card.jsx
+++ docs/src/components/lib/card.jsx
@@ -2,13 +2,11 @@ export default function Card() {
   return (
     <div class="va-card w-full">
       <div class="va-card__body">
-        <h2 class="va-card__title">Your Gateway to Timeless Literature</h2>
+        <h2 class="va-card__title">Timeless Literature</h2>
         <p>
-          Welcome to OMUSO API, a digital service for literature enthusiasts and
-          developrs. Our platform leverages a powerful API to bring you an
-          extensive collection of classical texts and detailed information about
-          their authors, making it easier than ever to delve into the world’s
-          most treasured literary works.
+          Our platform leverages a powerful API to bring you an extensive
+          collection of classical texts, making it easier than ever to delve
+          into the world’s most treasured literary works.
         </p>
       </div>
       <div class="va-card__actions text-right">
--- docs/src/components/lib/table.jsx
+++ docs/src/components/lib/table.jsx
@@ -10,15 +10,15 @@ export default function Table() {
       <tbody>
         <tr>
           <th>First 6 weeks</th>
-          <td>£109.80 per week</td>
+          <td>109.80€ per week</td>
         </tr>
         <tr>
           <th>Next 33 weeks</th>
-          <td>£109.80 per week</td>
+          <td>109.80€ per week</td>
         </tr>
         <tr>
           <th>Total estimated pay</th>
-          <td>£4,282.20</td>
+          <td>4,282.20€</td>
         </tr>
       </tbody>
     </table>
