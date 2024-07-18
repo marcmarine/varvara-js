f9c5bd76974adda788044e31094c86a662226b7a
Marc Mariné <shenobi@gmail.com>
2024-07-18 12:24:39 +0200

    feat(docs): Deploy docs to GitHub Pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,12 +3,10 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://varvara.js.org',
   integrations: [
     tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
       applyBaseStyles: false
     }),
     mdx(),
