860bbaa6e45e4d4769c1f7a876ef250dd72fc550
Marc Mariné <shenobi@gmail.com>
2024-10-07 18:48:54 +0200

    docs: Add documentation for Collapse component

---
+++ docs/src/components/lib/collapse.jsx
@@ -0,0 +1,15 @@
+export default function Collapse() {
+  return (
+    <details class="va-collapse w-full">
+      <summary>
+        <strong>The purpose of thought</strong>
+      </summary>
+      <p>
+        We can attain wisdom by three ways: first, by way of experience, this is
+        the most difficult way; second, by way of imitation, this is the easiest
+        way; and third by way of meditation, and this is the noblest way.
+      </p>
+      <p>Confucius</p>
+    </details>
+  )
+}
