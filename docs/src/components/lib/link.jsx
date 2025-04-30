227a95d45e89806b445f4cd9cf6bcbb3e023e81a
Marc Mariné <shenobi@gmail.com>
2025-04-30 21:14:54 +0200

    docs: Update documentation for v0.7.1

---
+++ docs/src/components/lib/link.jsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <a href="#" class="va-link">
+      Link
+    </a>
+  )
+}
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     borderColor: {
-      DEFAULT: 'var(--va-text-normal)'
+      DEFAULT: 'var(--va-foreground-color-default)'
     }
   }
 }
