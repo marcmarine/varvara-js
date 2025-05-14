43b5a4d47ead110556268aee26f8e5aab91a7ea0
Marc Mariné <shenobi@gmail.com>
2025-05-14 18:52:50 +0200

    style(docs): Update components page

--- docs/src/components/lib/card.jsx
+++ docs/src/components/lib/card.jsx
@@ -5,12 +5,11 @@ export default function Card() {
         <h2 class="va-card__title">Timeless Literature</h2>
         <p>
           Our platform leverages a powerful API to bring you an extensive
-          collection of classical texts, making it easier than ever to delve
-          into the world’s most treasured literary works.
+          collection...
         </p>
       </div>
       <div class="va-card__actions text-right">
-        <button class="va-button">Start →</button>
+        <button class="va-button">Start</button>
       </div>
     </div>
   )
