6b8233312dfaefe7bef733fa025e03db37479226
Marc Mariné <shenobi@gmail.com>
2025-05-20 20:57:14 +0200

    feat(css): Enable granular style imports for components

---
+++ .changeset/huge-candies-rush.md
@@ -0,0 +1,5 @@
+---
+"varvara-css": patch
+---
+
+Allow importing styles for individual components
--- packages/css/postcss.config.js
+++ packages/css/postcss.config.js
@@ -1,13 +1,11 @@
 import autoprefixer from 'autoprefixer'
 import nested from 'postcss-nested'
 import tailwindcss from 'tailwindcss'
-import atImport from 'postcss-import'
 import prefixer from 'postcss-prefixer'
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
-    atImport,
     nested,
     tailwindcss,
     autoprefixer,
